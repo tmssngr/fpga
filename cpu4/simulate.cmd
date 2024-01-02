4584a9a8297aaae82f0a19af4abcc479b32e5593
Thomas Singer <tmssngr@users.noreply.github.com>
2024-01-02 19:49:06 +0100

    simulate.cmd with comments

--- cpu4/simulate.cmd
+++ cpu4/simulate.cmd
@@ -20,7 +20,28 @@ echo %NAME%
 echo -------------
 if exist %NAME%.vcd del %NAME%.vcd
 
+:: Usage: iverilog [-EiRSuvV] [-B base] [-c cmdfile|-f cmdfile]
+::                 [-g1995|-g2001|-g2005|-g2005-sv|-g2009|-g2012] [-g<feature>]
+::                 [-D macro[=defn]] [-I includedir] [-L moduledir]
+::                 [-M [mode=]depfile] [-m module]
+::                 [-N file] [-o filename] [-p flag=value]
+::                 [-s topmodule] [-t target] [-T min|typ|max]
+::                 [-W class] [-y dir] [-Y suf] [-l file] source_file(s)
 iverilog -o %NAME_TB%.o -s test%NAME% %NAME%.v %NAME_TB%.v
+
+:: Usage: vvp [options] input-file [+plusargs...]
+:: Options:
+::  -h             Print this help message.
+::  -i             Interactive mode (unbuffered stdio).
+::  -l file        Logfile, '-' for <stderr>
+::  -M path        VPI module directory
+::  -M -           Clear VPI module path
+::  -m module      Load vpi module.
+::  -n             Non-interactive ($stop = $finish).
+::  -N             Same as -n, but exit code is 1 instead of 0
+::  -s             $stop right away.
+::  -v             Verbose progress messages.
+::  -V             Print the version information.
 vvp %NAME_TB%.o
 echo:
 goto :eof
