458d36a4fe8bb3de6d736a6e07de04752025fba1
Thomas Singer <tmssngr@users.noreply.github.com>
2024-01-13 10:38:32 +0100

    SoC_tb.v: align code

--- cpu4/SoC_tb.v
+++ cpu4/SoC_tb.v
@@ -2,12 +2,12 @@ module testSoC();
 
 `include "assert.vh"
 `include "alu.vh"
-`define assertPc(value) if (uut.proc.pc !== value) begin $display("ASSERTION FAILED in %m: pc(%h) != value", uut.proc.pc); $finish(2); end
-`define assertInstr(value) if (uut.proc.instruction !== value) begin $display("ASSERTION FAILED in %m: instruction(%h) != value", uut.proc.instruction); $finish(2); end
-`define assertSecond(value) if (uut.proc.second !== value) begin $display("ASSERTION FAILED in %m: second(%h) != value", uut.proc.second); $finish(2); end
-`define assertThird(value) if (uut.proc.third !== value) begin $display("ASSERTION FAILED in %m: third(%h) != value", uut.proc.third); $finish(2); end
-`define assertFlags(value) if (uut.proc.flags !== value) begin $display("ASSERTION FAILED in %m: flags != value", uut.proc.flags); $finish(2); end
-`define assertRegister(num, value) if (uut.proc.registers[num] !== value) begin $display("ASSERTION FAILED in %m: reg[%h] != value", num, uut.proc.registers[num]); $finish(2); end
+`define assertPc(value)            if (uut.proc.pc             !== value) begin $display("ASSERTION FAILED in %m: pc(%h) != value"         , uut.proc.pc                 ); $finish(2); end
+`define assertInstr(value)         if (uut.proc.instruction    !== value) begin $display("ASSERTION FAILED in %m: instruction(%h) != value", uut.proc.instruction        ); $finish(2); end
+`define assertSecond(value)        if (uut.proc.second         !== value) begin $display("ASSERTION FAILED in %m: second(%h) != value"     , uut.proc.second             ); $finish(2); end
+`define assertThird(value)         if (uut.proc.third          !== value) begin $display("ASSERTION FAILED in %m: third(%h) != value"      , uut.proc.third              ); $finish(2); end
+`define assertFlags(value)         if (uut.proc.flags          !== value) begin $display("ASSERTION FAILED in %m: flags != value"          , uut.proc.flags              ); $finish(2); end
+`define assertRegister(num, value) if (uut.proc.registers[num] !== value) begin $display("ASSERTION FAILED in %m: reg[%h] != value"        , num, uut.proc.registers[num]); $finish(2); end
 
     reg clk = 0;
 
